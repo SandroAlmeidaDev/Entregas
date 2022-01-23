e510b3d2b8ea062b6397e1520231fed9373ac7f7
Sandro Almeida da Silva <sandro.silva@gazin.com.br>
2022-01-23 17:01:16 -0300

    Created CreateClient

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,36 @@
+import { prisma } from "../../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateClient {
+  username: string;
+  password: string;
+}
+
+export class CreateClientUseCase {
+  async execute({username, password}: ICreateClient) {
+    // Validar se o client existe
+    const clientExist = await prisma.clients.findFirst({
+      where: {
+        username: {
+          endsWith: 'prisma.io',
+          mode: 'insensitive', 
+        }
+      }
+    })
+
+    if(clientExist) {
+      throw new Error(`Client ${username} already exists`);
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const client  = await prisma.clients.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return client;
+  }  
+}
