b693af92dc4ebe9dc3266a79b860374a40f8115a
Sandro Almeida da Silva <sandro.silva@gazin.com.br>
2022-01-23 22:12:50 -0300

    Created Deliveryman module

---
+++ src/modules/deliveryman/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,40 @@
+import { prisma } from "../../../database/prismaClient";
+import { hash } from "bcrypt";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({username, password}: ICreateDeliveryman) {
+    const deliverymanExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          endsWith: 'prisma.io',
+          mode: 'insensitive',
+        }
+      }
+    })
+
+    if(deliverymanExist) {
+      throw new Error(`Client ${username} already exists`);
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const deliveryman  = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+      select: {
+        id: true,
+        username: true,
+        password: false
+      }
+    })
+
+    return deliveryman;
+  }
+}
