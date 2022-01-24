ac67f168c2077456917fec2e41b3ab4a73a6ef14
Sandro Almeida da Silva <sandro.silva@gazin.com.br>
2022-01-23 22:58:57 -0300

    Created Delivery module

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/createDeliveryman/createDeliverymanController';
 
 const routes = Router();
@@ -9,9 +10,8 @@ const routes = Router();
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
-
-
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 routes.post(
@@ -20,5 +20,6 @@ routes.post(
 );
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+routes.post('/delivery', createDeliveryController.handle);
 
 export { routes }
